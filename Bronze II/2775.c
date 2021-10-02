2c24524f59cbea4ab13efa934d599e9a6e371087
white201 <white201.dev@gmail.com>
2021-10-02 21:23:25 +0900

    2021.10.02

---
+++ Bronze II/2775.c
@@ -0,0 +1,20 @@
+//2775  부녀회장이 될테야
+
+#include <stdio.h>
+
+int get(int a, int b) {
+    if(a==0) return b;
+    int result=0;
+    for(int i=0;i<b;i++) result += get(a-1,b-i);
+    return result;
+}
+
+int main() {
+    int T;scanf("%d",&T);
+    int n,k;
+    for(int i=0;i<T;i++) {
+        scanf("%d %d",&k,&n);
+        printf("%d",get(k,n));
+    }
+    return 0;
+}
