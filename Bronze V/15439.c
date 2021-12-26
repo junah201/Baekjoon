920868f177ff0d2a0b4c20962b312eb9e5e6e4e9
white201 <junah.dev@gmail.com>
2021-12-27 00:12:30 +0900

    2021.12.27

---
+++ Bronze V/15439.c
@@ -0,0 +1,11 @@
+//15439  Vera and Outfits
+
+#include <stdio.h>
+
+int N;
+
+int main() {
+    scanf("%d",&N);
+    printf("%d",N*N-N);
+    return 0;
+}
---
+++ Bronze V/18096.c
@@ -0,0 +1,8 @@
+//18096  Арифметическая магия
+
+#include <stdio.h>
+
+int main() {
+    printf("1");
+    return 0;
+}
