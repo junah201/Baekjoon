86f76699b82b28662c82ba894a7a68dbc7930c11
white201 <white_201@naver.com>
2022-01-24 00:44:19 +0900

    2022.01.24

---
+++ Bronze IV/10707.py
@@ -0,0 +1,14 @@
+#10707  수도 요금
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+P = int(input())
+
+if P > C:
+    result = B + (P-C)*D
+else:
+    result = B
+
+print(min(A*P, result))
