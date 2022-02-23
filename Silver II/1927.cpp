d3d90d7278a57f820093a27c48c2149f430060e1
junah201 <junah.dev@gmail.com>
2022-02-23 15:55:47 +0900

    2022.02.23

---
+++ Silver II/1927.cpp
@@ -0,0 +1,32 @@
+// 1927 최소 힙
+// https://www.acmicpc.net/source/39478903
+
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int T, N;
+
+int main()
+{
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        if (!N)
+        {
+            if (pq.empty())
+                printf("0\n");
+            else
+            {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        }
+        else
+            pq.push(N);
+    }
+    return 0;
+}
