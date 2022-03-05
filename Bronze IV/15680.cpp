194252b5b4d978ef977cd3ff1211521223d822f2
junah201 <junah.dev@gmail.com>
2022-03-06 03:37:20 +0900

    2022.03.06

---
+++ Bronze IV/15680.cpp
@@ -0,0 +1,17 @@
+// 15680 연세대학교
+// https://www.acmicpc.net/source/39999654
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main()
+{
+    scanf("%d", &N);
+    if (N == 0)
+        printf("YONSEI");
+    else
+        printf("Leading the Way to the Future");
+    return 0;
+}
---
+++ Bronze IV/15700.py
@@ -0,0 +1,5 @@
+# 15700 타일 채우기 4
+# https://www.acmicpc.net/source/39937661
+
+N, M = map(int, input().split())
+print((N * M) // 2);
