5a8de2d7e1e2f48e0419f9e92a9ea361fa8d2716
junah <junah.dev@gmail.com>
2022-06-05 16:25:07 +0900

    2022.06.05

--- Gold I/24914.cpp
+++ Gold I/24914.cpp
@@ -5,6 +5,17 @@ int N, M, Q, cnt;
 vector<vector<pair<int, int>>> graph;
 vector<pair<int, pair<int, int>>> node;
 
+void print_graph() {
+    for(int i=1;i<=N;i++) {
+        printf("=========\n");
+        printf("%d\n", i);
+        for(auto [idx, color] : graph[i]) {
+            printf("%d => %d\n", idx, color);
+        }
+    }
+}
+
+
 int main() {
     scanf("%d %d %d", &N, &M, &Q);
     graph.resize(N+1);
@@ -39,21 +50,24 @@ int main() {
         scanf("%d %d", &idx, &color);
         auto [orgin_color, tmp] = node[idx - 1];
         auto [start, end] = tmp;
-    
-        if(orgin_color == color) {
-            printf("%d\n", cnt);
-            continue;
-        }
 
         int orgin_S = 0, orgin_E = 0;
         int change_S = 0, change_E = 0;
+
+        if(orgin_color == color) 
+        {
+            printf("%d\n" ,cnt);
+            continue;
+        }
+
+        // print_graph();
 
         for(auto [tidx, tcolor] : graph[start]) {
             if(tcolor == orgin_color) {
                 orgin_S++;
             }
             if(tcolor == color) {
-                change_S = 1;
+                change_S++;
             }
         }
 
@@ -62,11 +76,9 @@ int main() {
                 orgin_E++;
             }
             if(tcolor == color) {
-                change_E = 1;
+                change_E++;
             }
         }
-        //printf("=%d %d\n",start, end);
-        //printf("(%d, %d) (%d %d)\n", orgin_S >= 2, orgin_E >= 2, change_S, change_E);
 
         if(orgin_S >= 2 &&  orgin_E >= 2)
             cnt += 1;
@@ -75,7 +87,7 @@ int main() {
         else
             cnt -= 1;
 
-        if(change_S + change_E == 2)
+        if(change_S && change_E)
             cnt -= 1;
         else if(change_S + change_E == 1)
             cnt += 0;
@@ -83,7 +95,8 @@ int main() {
             cnt += 1;
 
         printf("%d\n", cnt);
-    
+
+        node[idx - 1].first = color;
         for(int i=0;i<graph[start].size();i++)
             if(graph[start][i].first == end) {
                 graph[start][i].second = color;
