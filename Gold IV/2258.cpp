4e51f8b1d0ff420a992f11855564664b27834ca4
junah201 <junah.dev@gmail.com>
2022-12-20 14:18:45 +0900

    2022.12.20

--- Gold IV/2258.cpp
+++ Gold IV/2258.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, ans = INT_MAX;
-vector<pair<int, vector<int>>> sorted_meat;
-map<int, int> price_meat;
+int N, M, ans = -1;
+vector<pair<int, int>> meat;
+int total_w, last_c, total_c;
 
 int main()
 {
@@ -11,41 +11,29 @@ int main()
     for (int i = 0, w, c; i < N; i++)
     {
         scanf("%d %d", &w, &c);
-        sorted_meat.emplace_back(c, w);
+        meat.emplace_back(c, w);
     }
-    sort(sorted_meat.begin(), sorted_meat.end());
+    sort(meat.begin(), meat.end(), [](const pair<int, int> &a, const pair<int, int> &b)
+         {
+        auto [c1, w1] = a;
+        auto[c2, w2] = b;
+            if(c1 == c2) return w1 > w2;
+            return c1 < c2; });
 
-    for (int i = 0; i < N; i++)
+    for (auto [c, w] : meat)
     {
-        auto [c, w] = sorted_meat[i];
-        if (price_meat.find(c) == price_meat.end())
-        {
-            price_meat.emplace(w);
-        }
-        else
-        {
-            price_meat[c].emplace_back(w);
-        }
-    }
+        if (last_c != c)
+            total_c = 0;
+        total_c += c;
+        last_c = c;
+        total_w += w;
 
-    int tmp = 0;
-    for (int i = 0; i < N; i++)
-    {
-        auto [c, w] = meat[i];
-        if (i != 0 && meat[i].first == meat[i - 1].first)
-            continue;
-        if (tmp + w >= M)
+        if (total_w >= M)
         {
-            ans = c;
-            break;
+            if (ans == -1)
+                ans = total_c;
+            ans = min(ans, total_c);
         }
-        tmp += w;
     }
-    if (ans)
-        printf("%d", ans);
-    else
-        printf("%d", -1);
+    printf("%d", ans);
 }
-
-// https://s.search.naver.com/p/intentblock/search.naver?ac=0&aq=0&bid=SYS-0000000001783649&display=10&fgn_city=&fgn_region=&lgl_lat=37.373382&lgl_long=126.938588&lgl_rcode=02410104&ngn_country=KR&nlu_query=%7B%22v%22%3A%223%22%2C%22intentblock%22%3A%221%22%2C%22qid%22%3A%22%EB%B6%88%EA%B4%91%EC%97%AD%EB%A7%9B%EC%A7%91%22%2C%22abt%22%3A%5B%7B%22eid%22%3A%22SBR1%22%2C%22value%22%3A%7B%22bucket%22%3A%22T2%22%2C%22is_control%22%3Afalse%7D%7D%5D%7D&query=%EB%B6%88%EA%B4%91%EC%97%AD+%EB%A7%9B%EC%A7%91&ssc=tab.itb.all&start=1&where=nx_bridge
-// https://s.search.naver.com/p/intentblock/search.naver?ac=0&aq=0&bid=SYS-0000000001754221&display=10&fgn_city=&fgn_region=&lgl_lat=37.373382&lgl_long=126.938588&lgl_rcode=02410104&ngn_country=KR&nlu_query=%7B%22v%22%3A%223%22%2C%22intentblock%22%3A%221%22%2C%22qid%22%3A%22%EB%B6%88%EA%B4%91%EC%97%AD%EB%A7%9B%EC%A7%91%22%2C%22abt%22%3A%5B%7B%22eid%22%3A%22SBR1%22%2C%22value%22%3A%7B%22bucket%22%3A%22T2%22%2C%22is_control%22%3Afalse%7D%7D%5D%7D&query=%EB%B6%88%EA%B4%91%EC%97%AD+%EB%A7%9B%EC%A7%91&ssc=tab.itb.all&start=1&where=nx_bridge
