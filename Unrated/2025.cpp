c0072bb608a84f436c4f6fd72dafc8b28460ba01
junah201 <junah.dev@gmail.com>
2023-01-05 01:35:10 +0900

    2022.01.05

--- Unrated/2025.cpp
+++ Unrated/2025.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int N;
 
-int dx[8] = {1,1,-1,-1,2,2,-2,-2};
-int dy[8] = {2,-2,2,-2,1,-1,1,-1};
+int dx[8] = {1, 1, -1, -1, 2, 2, -2, -2};
+int dy[8] = {2, -2, 2, -2, 1, -1, 1, -1};
 
-struct block {
+struct block
+{
     int x;
     int y;
     int cnt;
 } input;
 
-int main() {
+int main()
+{
     scanf("%d", &N);
     scanf("%d %d", &input.x, &input.y);
 }
