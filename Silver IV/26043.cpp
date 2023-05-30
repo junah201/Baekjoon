de2c9c5ffa72e2c4a756d4a8f7f60e8841821bb0
junah201~ <junah.dev@gmail.com>
2023-05-30 20:02:48 +0900

    2023.05.30

--- Silver IV/26043.cpp
+++ Silver IV/26043.cpp
@@ -14,23 +14,87 @@ void dprintf(const char *__format, ...)
 #endif
 }
 
-int cnt;
+int N;
+
+queue<pair<int, int>> student;
+queue<int> food;
+priority_queue<int, vector<int>, greater<int>> A, B, C;
 
 signed main()
 {
-    for (int i = 0, cnt; i < 3; i++)
+    scanf("%lld", &N);
+
+    while (N--)
     {
-        cnt = 0;
-        for (int j = 0, tmp; j < 4; j++)
+        int oper, a, b;
+        scanf("%lld", &oper);
+
+        if (oper == 1)
         {
-            scanf("%lld", &tmp);
-            cnt += !tmp;
+            scanf("%lld %lld", &a, &b);
+            student.emplace(a, b);
+        }
+        else if (oper == 2)
+        {
+            scanf("%lld", &b);
+            food.emplace(b);
         }
 
-        if (cnt == 0)
-            printf("E\n");
-        else
-            printf("%c\n", 'A' + cnt - 1);
+        while (!student.empty() && !food.empty())
+        {
+            auto [id, favo] = student.front();
+            student.pop();
+
+            int now_food = food.front();
+            food.pop();
+
+            if (now_food == favo)
+                A.emplace(id);
+            else
+                B.emplace(id);
+        }
     }
+
+    if (A.empty())
+        printf("None\n");
+    else
+    {
+        while (!A.empty())
+        {
+            printf("%lld ", A.top());
+            A.pop();
+        }
+        printf("\n");
+    }
+
+    if (B.empty())
+        printf("None\n");
+    else
+    {
+        while (!B.empty())
+        {
+            printf("%lld ", B.top());
+            B.pop();
+        }
+        printf("\n");
+    }
+
+    if (student.empty())
+        printf("None\n");
+    else
+    {
+        while (!student.empty())
+        {
+            C.emplace(student.front().first);
+            student.pop();
+        }
+        while (!C.empty())
+        {
+            printf("%lld ", C.top());
+            C.pop();
+        }
+        printf("\n");
+    }
+
     return 0;
 }
