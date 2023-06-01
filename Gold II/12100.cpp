9fcc04a42adc140ca0df79bfb767570d5852566b
junah201~ <junah.dev@gmail.com>
2023-06-02 04:33:54 +0900

    2023.06.02

--- Gold II/12100.cpp
+++ Gold II/12100.cpp
@@ -14,20 +14,178 @@ void dprintf(const char *__format, ...)
 #endif
 }
 
-int N;
-vector<vector<int>> inp;
+int N, ans;
+vector<vector<int>> init_inp;
+// U D R L
+vector<vector<int>> move(int dir, vector<vector<int>> inp)
+{
+    vector<vector<int>> checker(N, vector<int>(N, false));
+    // U
+    if (dir == 0)
+    {
+        for (int y = 0; y < N; y++)
+        {
+            for (int x = 0; x < N; x++)
+            {
+                int init_value = inp[y][x];
+                for (int ny = y - 1; ny >= 0; ny--)
+                {
+                    if (checker[ny][x])
+                        break;
+                    if (inp[ny][x] == 0)
+                    {
+                        inp[ny][x] = inp[ny + 1][x];
+                        inp[ny + 1][x] = 0;
+                    }
+                    else if (inp[ny][x] == init_value)
+                    {
+                        inp[ny][x] *= 2;
+                        inp[ny + 1][x] = 0;
+                        checker[ny][x] = true;
+                        break;
+                    }
+                    else if (inp[ny][x] != init_value)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // D
+    else if (dir == 1)
+    {
+        for (int y = N - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < N; x++)
+            {
+                int init_value = inp[y][x];
+                for (int ny = y + 1; ny < N; ny++)
+                {
+                    if (checker[ny][x])
+                        break;
+                    if (inp[ny][x] == 0)
+                    {
+                        inp[ny][x] = inp[ny - 1][x];
+                        inp[ny - 1][x] = 0;
+                    }
+                    else if (inp[ny][x] == init_value)
+                    {
+                        inp[ny][x] *= 2;
+                        inp[ny - 1][x] = 0;
+                        checker[ny][x] = true;
+                        break;
+                    }
+                    else if (inp[ny][x] != init_value)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // R
+    else if (dir == 2)
+    {
+        for (int x = 0; x < N; x++)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                int init_value = inp[y][x];
+
+                for (int nx = x - 1; nx >= 0; nx--)
+                {
+                    if (checker[y][nx])
+                        break;
+                    if (inp[y][nx] == 0)
+                    {
+                        inp[y][nx] = inp[y][nx + 1];
+                        inp[y][nx + 1] = 0;
+                    }
+                    else if (inp[y][nx] == init_value)
+                    {
+                        inp[y][nx] *= 2;
+                        inp[y][nx + 1] = 0;
+                        checker[y][nx] = true;
+                        break;
+                    }
+                    else if (inp[y][nx] != init_value)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // L
+    else if (dir == 3)
+    {
+        for (int x = N - 1; x >= 0; x--)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                int init_value = inp[y][x];
+                for (int nx = x + 1; nx < N; nx++)
+                {
+                    if (checker[y][nx])
+                        break;
+                    if (inp[y][nx] == 0)
+                    {
+                        inp[y][nx] = inp[y][nx - 1];
+                        inp[y][nx - 1] = 0;
+                    }
+                    else if (inp[y][nx] == init_value)
+                    {
+                        inp[y][nx] *= 2;
+                        inp[y][nx - 1] = 0;
+                        checker[y][nx] = true;
+                        break;
+                    }
+                    else if (inp[y][nx] != init_value)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return inp;
+}
+
+void dfs(int cnt, vector<vector<int>> inp)
+{
+    if (cnt >= 5)
+    {
+        for (int y = 0; y < N; y++)
+            for (int x = 0; x < N; x++)
+                ans = max(ans, inp[y][x]);
+
+        return;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        vector<vector<int>> moved_inp = move(i, inp);
+        dfs(cnt + 1, moved_inp);
+    }
+}
 
 signed main()
 {
     scanf("%lld", &N);
-    inp.resize(N, vector<int>(N));
+    init_inp.resize(N, vector<int>(N));
     for (int y = 0; y < N; y++)
     {
         for (int x = 0; x < N; x++)
         {
-            scanf("%lld", &inp[y][x]);
+            scanf("%lld", &init_inp[y][x]);
         }
     }
 
-        return 0;
+    dfs(0, init_inp);
+
+    printf("%lld", ans);
+
+    return 0;
 }
