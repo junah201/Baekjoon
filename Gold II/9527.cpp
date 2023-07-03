b7e10c3fb2d61dcfa7313ce715f7a8aa0abe3341
junah201~ <junah.dev@gmail.com>
2023-07-04 03:43:16 +0900

    2023.07.04

--- Gold II/9527.cpp
+++ Gold II/9527.cpp
@@ -1,9 +1,68 @@
+/* Code By [ junah ]
+GitHub : [ junah201 ] */
 #include <bits/stdc++.h>
 using namespace std;
+#define all(v) v.begin(), v.end()
+#define int long long int
+#define INF LONG_LONG_MAX
 
-int A, B, sum[];
-
-int main()
+signed main()
 {
-    scanf("%d %d", &A, &B);
+    vector<int> bits, bits_sum;
+
+    int tmp = 0;
+
+    for (int i = 0; i <= 20; i++)
+    {
+        bits.emplace_back(1 << i);
+        tmp = tmp | 1 << i;
+        bits_sum.emplace_back(tmp);
+        printf("%lld ", bits[i]);
+    }
+
+    int A, B;
+    scanf("%lld", &A, &B);
+    vector<int> sum(B + 1, 0);
+
+    printf("\n");
+    for (int i = 1; i <= B; i++)
+    {
+        sum[i] = sum[i - 1];
+        for (int j = 0; j <= 20; j++)
+        {
+            printf("%lld %lld : %lld\n", i, j, bits[j] & i);
+            sum[i] += (int)((bits[j] & i) != 0);
+        }
+        printf("= %lld : %lld\n", i, sum[i]);
+    }
+
+    printf("%lld", sum[B] - sum[A - 1]);
+
+    return 0;
+}
+
+/* Code By [ junah ]
+GitHub : [ junah201 ] */
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(), v.end()
+#define int long long int
+#define INF LONG_LONG_MAX
+
+void dprintf(const char *__format, ...)
+{
+#ifndef ONLINE_JUDGE
+    va_list args;
+    va_start(args, __format);
+    vprintf(__format, args);
+    va_end(args);
+#endif
+}
+
+int N;
+
+signed main()
+{
+    scanf("%lld", &N);
+    return 0;
 }
--- Gold V/14503.cpp
+++ Gold V/14503.cpp
@@ -1,36 +1,107 @@
-#include <stdio.h>
-
+/* Code By [ junah ]
+GitHub : [ junah201 ] */
+#include <bits/stdc++.h>
 using namespace std;
-
-int N, M;
-int R, C, D;
-int map[52][52];
+#define all(v) v.begin(), v.end()
+#define int long long int
+#define INF LONG_LONG_MAX
 
 int dx[4] = {0, 1, 0, -1};
-int dy[4] = {-1, 0, 1, 1};
+int dy[4] = {1, 0, -1, 0};
 
-int main()
+int X, Y;
+int sx, sy, sd;
+int ans = 0;
+
+vector<vector<int>> inp;
+
+bool check_waste(int x, int y)
 {
-    scanf("%d %d", &N, &M);
-    scanf("%d %d %d", &R, &C, &D);
-    for (int y = 0; y < N; y++)
-        for (int x = 0; x < N; x++)
-            scanf("%d", &map[y][x]);
-    while (true)
+    for (int i = 0; i < 4; i++)
     {
-        // 1번 쿼리
-        if (map[C][R] == 0)
-            map[C][R] = 2;
-        // 2 쿼리 - a
-        D = (D + 1) % 4;
-        int rx = R + dx[D];
-        int ry = C + dy[D];
-        if (map[ry][rx] == 0)
-        {
-            R = rx;
-            C = ry;
-        }
-        else
-            continue;
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (inp[ny][nx] == 0)
+            return true;
     }
+    return false;
+}
+
+void print_inp(int xx, int yy, int dd)
+{
+    printf("==== (%lld, %lld) %lld ====\n", xx, yy, dd);
+    for (int y = 0; y < Y; y++)
+    {
+        for (int x = 0; x < X; x++)
+        {
+            if (x == xx && y == yy)
+            {
+                printf("■");
+            }
+            else
+                printf("%lld", inp[y][x]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+signed main()
+{
+    scanf("%lld %lld", &Y, &X);
+    scanf("%lld %lld %lld", &sx, &sy, &sd);
+
+    inp.resize(Y, vector<int>(X, 0));
+
+    for (int y = 0; y < Y; y++)
+        for (int x = 0; x < X; x++)
+            scanf("%lld", &inp[y][x]);
+
+    int x = sx, y = sy, d = sd;
+
+    while (true)
+    // for (int k = 0; k < 20; k++)
+    {
+        print_inp(x, y, d);
+        // 1. 현재 칸 청소
+        if (inp[y][x] == 0)
+        {
+            printf("= 1\n");
+            inp[y][x] = 2;
+            ans++;
+        }
+        // 3. 현재 칸의 주변 4칸에 청소되지 않는 빈칸이 있는 경우
+        else if (check_waste(x, y))
+        {
+            printf("= 3\n");
+            d = (d - 1 + 4) % 4;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (inp[ny][nx] == 0)
+            {
+                x = nx;
+                y = ny;
+            }
+        }
+        // 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
+        else
+        {
+            printf("= 2\n");
+            int nx = x - dx[d];
+            int ny = y - dy[d];
+
+            // 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
+            if (inp[ny][nx] == 1)
+                break;
+
+            x = nx;
+            y = ny;
+        }
+    }
+
+    printf("%lld", ans);
+
+    return 0;
 }
